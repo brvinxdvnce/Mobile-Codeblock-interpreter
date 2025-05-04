9068b020cf110f791e85bf12b6cd475f765e8427
brvinxdvnce <ivanovdanil.hits2024@gmail.com>
2025-05-05 02:25:30 +0300

    .

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,64 +4,106 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.expandHorizontally
 import androidx.compose.foundation.background
+import androidx.compose.foundation.content.contentReceiver
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.compositionLocalWithComputedDefaultOf
+import androidx.compose.foundation.layout.size
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Bar()
-//            MyApplicationTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
+            val navController = rememberNavController()
+            NavHost (navController = navController, startDestination = "info") {
+                composable("play") {
+                    Play(navController)
+                }
+                composable("ready") {
+                    ReadyScripts(navController)
+                }
+                composable("info") {
+                    Info(navController)
+                }
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Bar() {
+fun Bar (navController: NavHostController) {
     Row (modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 32.dp)
-            .background(Color.Gray),
+        .fillMaxWidth()
+        .padding(top = 32.dp)
+        .background(Color.Gray),
         horizontalArrangement = Arrangement
             .SpaceEvenly
     )
     {
-        Button(onClick  = {}) { Text("play") }
-        Button(onClick  = {}) { Text("scripts") }
-        Button(onClick  = {}) { Text("button") }
-        Button(onClick  = {}) { Text("settings") }
+        Button(onClick  = { navController.navigate("play" ) }) { Text("play") }
+        Button(onClick  = { navController.navigate("ready") }) { Text("scripts") }
+        Button(onClick  = { navController.navigate("info" ) }) { Text("info") }
     }
 }
+
+@Composable
+fun ReadyScripts (navController: NavHostController) {
+    Bar(navController)
+}
+
+@Composable
+fun Info (navController: NavHostController) {
+    Box (modifier = Modifier.fillMaxSize()) {
+        Bar(navController)
+
+        Box (
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Box (
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(Color.Gray),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("hi", textAlign = TextAlign.Center)
+            }
+        }
+
+//        Column (
+//            modifier = Modifier.fillMaxSize(),
+//
+//        ) {
+//            Text("dv",
+//                modifier = Modifier.fillMaxSize(),
+//                textAlign = TextAlign.Center)
+//        }
+    }
+}
+
+@Composable
+fun Play (navController: NavHostController) {
+    Bar(navController)
+
+
+}
+

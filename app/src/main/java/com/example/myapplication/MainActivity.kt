65bc49e79d33a0d9f2ade0b51c08adf9abb1cf6d
brvinxdvnce <ivanovdanil.hits2024@gmail.com>
2025-05-06 19:10:02 +0300

    ui add pages

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -26,7 +26,15 @@ import androidx.navigation.compose.*
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.compositionLocalWithComputedDefaultOf
 import androidx.compose.foundation.layout.size
-
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,51 +67,161 @@ fun Bar (navController: NavHostController) {
             .SpaceEvenly
     )
     {
-        Button(onClick  = { navController.navigate("play" ) }) { Text("play") }
-        Button(onClick  = { navController.navigate("ready") }) { Text("scripts") }
-        Button(onClick  = { navController.navigate("info" ) }) { Text("info") }
+        Button(onClick  = { navController.navigate("play" ) }) { Text("Try solve") }
+        Button(onClick  = { navController.navigate("ready") }) { Text("Ready scripts") }
+        Button(onClick  = { navController.navigate("info" ) }) { Text("Information") }
     }
 }
 
 @Composable
 fun ReadyScripts (navController: NavHostController) {
-    Bar(navController)
-}
-
-@Composable
-fun Info (navController: NavHostController) {
-    Box (modifier = Modifier.fillMaxSize()) {
+    Column () {
         Bar(navController)
 
-        Box (
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Box (
-                modifier = Modifier
-                    .size(100.dp)
-                    .background(Color.Gray),
-                contentAlignment = Alignment.Center
-            ) {
-                Text("hi", textAlign = TextAlign.Center)
-            }
+        Box() {
+            Button(onClick = {
+                navController.navigate("play" )
+                // генерация скрипта
+            }) { Text("Bubble sort") }
         }
-
-//        Column (
-//            modifier = Modifier.fillMaxSize(),
-//
-//        ) {
-//            Text("dv",
-//                modifier = Modifier.fillMaxSize(),
-//                textAlign = TextAlign.Center)
-//        }
+        Box() {
+            Button(onClick = {
+                navController.navigate("play" )
+                // генерация скрипта
+            }) { Text("algos") }
+        }
+        Box() {
+            Button(onClick = {
+                navController.navigate("play" )
+                // генерация скрипта
+            }) { Text("algos") }
+        }
+        Box() {
+            Button(onClick = {
+                navController.navigate("play" )
+                // генерация скрипта
+            }) { Text("algos") }
+        }
     }
 }
 
 @Composable
-fun Play (navController: NavHostController) {
+fun SetScript (name: String) {
+    Text(name)
+    Button(onClick = {
+        // переход в окно play ->
+        // генерация скрипта
+    }) { }
+}
+
+@Composable
+fun Info (navController: NavHostController) {
     Bar(navController)
 
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement
+            .SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box (
+            modifier = Modifier
+                .width((LocalConfiguration.current.screenWidthDp * 0.95).dp)
+                .background(Color.Gray),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Это мобильное приложение помогает легко создавать и запускать алгоритмы прямо на вашем телефоне. Вы строите алгоритмы из простых блоков - например, условий и циклов - и сразу видите, как они работают.\n" +
+                    "\n" +
+                    "Приложение не требует сложных настроек и компиляции - оно сразу выполняет ваш алгоритм, показывая результат пошагово. Это удобно для обучения и быстрого тестирования идей.",
+                textAlign = TextAlign.Center)
+        }
+
+        Box (
+            modifier = Modifier
+                .width((LocalConfiguration.current.screenWidthDp * 0.95).dp)
+                .background(Color.Gray),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Это мобильное приложение помогает легко создавать и запускать алгоритмы прямо на вашем телефоне. Вы строите алгоритмы из простых блоков - например, условий и циклов - и сразу видите, как они работают.\n" +
+                    "\n" +
+                    "Приложение не требует сложных настроек и компиляции - оно сразу выполняет ваш алгоритм, показывая результат пошагово. Это удобно для обучения и быстрого тестирования идей.",
+                textAlign = TextAlign.Center)
+        }
+
+    }
+}
+
+
+@Composable
+fun Play (navController: NavHostController) {
+    Column {
+        Bar(navController)
+        AddElemBar()
+    }
 
 }
 
+@Composable
+fun AddElemBar () {
+    var expanded by remember { mutableStateOf(false) }
+    Column {
+        Button(onClick = {expanded = !expanded}) {
+            Text(text = "add elem")
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {expanded = false}
+        ) {
+            DropdownMenuItem(
+                text = {Text("tmp")},
+                onClick = {
+                    //add elem from back
+                    expanded = false
+                }
+            )
+            DropdownMenuItem(
+                text = {Text("array")},
+                onClick = {
+                    //add elem from back
+                    expanded = false
+                }
+            )
+            DropdownMenuItem(
+                text = {Text("arithmetic")},
+                onClick = {
+                    //add elem from back
+                    expanded = false
+                }
+            )
+            DropdownMenuItem(
+                text = {Text("if")},
+                onClick = {
+                    //add elem from back
+                    expanded = false
+                }
+            )
+            DropdownMenuItem(
+                text = {Text("else if")},
+                onClick = {
+                    //add elem from back
+                    expanded = false
+                }
+            )
+            DropdownMenuItem(
+                text = {Text("compare")},
+                onClick = {
+                    //add elem from back
+                    expanded = false
+                }
+            )
+            DropdownMenuItem(
+                text = {Text("for")},
+                onClick = {
+                    //add elem from back
+                    expanded = false
+                }
+            )
+        }
+    }
+}
